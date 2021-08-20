9c335da94da65555c1482a75e9883f9c72d4d238
Rodrigo Dillon <rodrigodillon5@gmail.com>
2021-08-20 18:33:50 -0300

    1.3.0

--- src/main/java/com/example/financas/api/controller/LancamentoController.java
+++ src/main/java/com/example/financas/api/controller/LancamentoController.java
@@ -61,6 +61,12 @@ public class LancamentoController {
 	}
 	
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento ( @PathVariable("id") Long id) {
+		return service.obterPorId(id)
+					  .map( lancamento -> new ResponseEntity(converter(lancamento) , HttpStatus.OK))
+					  .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
 	
 	
 	@PostMapping
@@ -100,6 +106,21 @@ public class LancamentoController {
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}).orElseGet(() -> new ResponseEntity("Lancamento nao encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+						.id(lancamento.getId())
+						.descricao(lancamento.getDescricao())
+						.valor(lancamento.getValor())
+						.mes(lancamento.getMes())
+						.ano(lancamento.getAno())
+						.status(lancamento.getStatus().name())
+						.tipo(lancamento.getTipo().name())
+						.usuario(lancamento.getUsuario().getId())
+						.build();
+	}
+	
+	
 	
 	@PutMapping("{id}/atualiza-status")
 	public ResponseEntity atualizarStatus(@PathVariable Long id ,@RequestBody atualizaStatusDTO dto) {
--- src/main/java/com/example/financas/model/repository/LancamentoRepository.java
+++ src/main/java/com/example/financas/model/repository/LancamentoRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.financas.model.entity.Lancamento;
+import com.example.financas.model.enums.StatusLancamento;
 import com.example.financas.model.enums.TipoLancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 
-	@Query(value = "select sum(l.valor) from Lancamento l join l.usuario u where u.id = :idUsuario and l.tipo =:tipo group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
+	@Query(value = "select sum(l.valor) from Lancamento l join l.usuario u where u.id = :idUsuario and l.tipo =:tipo and l.status = :status group by u")
+	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo, @Param("status") StatusLancamento status);
 }
